90e95530804ef2f7f81513711b7ca53ce81e132f
Leorasz <scaevola1337@duck.com>
2023-10-08 20:15:30 -0600

    Removed useless files

--- model.py
+++ model.py
@@ -1,20 +1,23 @@
+
+import random
+random.seed(1337)
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import numpy as np
-import random
 
-# np.random.seed(1337)
-# torch.manual_seed(1337)
-# random.seed(1337)
+np.random.seed(1337)
+torch.manual_seed(1337)
+
 
 print("Finished imports")
 
-with open('input.txt', 'r', encoding='utf-8') as f:
-    text = f.read() # get text
+# with open('input.txt', 'r', encoding='utf-8') as f:
+#     text = f.read() # get text
 
-# text = ["bac","bab","abac","babac"]
+text = ["bac","bab","abac","babac"]
+text = ["a","b","c"]
 #given ba, 50/50 
 tokens = sorted(list(set("".join(text))))
 vocab_size = len(tokens) + 1
@@ -41,35 +44,35 @@ def oneHott(x):
     res[cid[x]] = 1
     return res
 
-# for i in text:
-#     rec = []
-#     for j in i:
-#         rec.append(oneHott(j))
-#     rec.append(end)
-#     input.append(rec)
-
 for i in text:
-    if i == "\n":
-        if wTFN:
-            wTFN = False
-            recording = True
-        if double:
-            recording = False
-            wTFN = True
-            record.append(end)
-            inputy.append(record[1:])
-            record = []
-        #     if not once:
-        #         once= True
-        #     else:
-        #         break
-        double = not double
-    if double and i != "\n":
-        double = False
-    if recording:
-        record.append(oneHott(i)) if i != " " else None
+    rec = []
+    for j in i:
+        rec.append(oneHott(j))
+    rec.append(end)
+    inputy.append(rec)
 
-inputy = inputy[1:]
+# for i in text:
+#     if i == "\n":
+#         if wTFN:
+#             wTFN = False
+#             recording = True
+#         if double:
+#             recording = False
+#             wTFN = True
+#             record.append(end)
+#             inputy.append(record[1:])
+#             record = []
+#         #     if not once:
+#         #         once= True
+#         #     else:
+#         #         break
+#         double = not double
+#     if double and i != "\n":
+#         double = False
+#     if recording:
+#         record.append(oneHott(i)) if i != " " else None
+
+# inputy = inputy[1:]
 print("Finished data processing")
 
 class LSTM(nn.Module):
@@ -77,17 +80,20 @@ class LSTM(nn.Module):
         super().__init__()
 
         self.forget_gate = nn.Linear(
-            input_size + hidden_size, hidden_size, bias=True
+            input_size + hidden_size, hidden_size, bias=True, dtype=torch.float64
         )  # set up linear transforms for the gates
         self.input_gate_forget = nn.Linear(
-            input_size + hidden_size, hidden_size, bias=True
+            input_size + hidden_size, hidden_size, bias=True, dtype=torch.float64
+
         )
         self.input_gate_candidate = nn.Linear(
-            input_size + hidden_size, hidden_size, bias=True
-        )
-        self.output_gate = nn.Linear(input_size + hidden_size, hidden_size, bias=True)
+            input_size + hidden_size, hidden_size, bias=True, dtype=torch.float64
 
-        self.output = nn.Linear(hidden_size, output_size)  # output linear layer
+        )
+        self.output_gate = nn.Linear(input_size + hidden_size, hidden_size, bias=True, dtype=torch.float64)
+
+
+        self.output = nn.Linear(hidden_size, output_size, dtype=torch.float64)  # output linear layer
 
         self.h_s = torch.zeros(1, hidden_size)  # hidden state/short-term memory
         self.c_s = torch.zeros(1, hidden_size)  # cell state/long-term memory
@@ -129,7 +135,7 @@ class Model(nn.Module):
         self.model = LSTM(input_size, hidden_size, input_size)
         self.iis = input_size
         self.os = output_size
-        self.sprobs = [0]*input_size
+        self.sprobs = [1e-7]*input_size
 
     def trainSequence(
         self,
@@ -161,9 +167,9 @@ class Model(nn.Module):
                         self.sprobs[pos] += 1
                         start = False
                         continue
-                    self.model.forward(torch.tensor(line[char], dtype=torch.float32).view(1,-1))
+                    self.model.forward(torch.tensor(line[char], dtype=torch.float64).view(1,-1))
                     output = self.model.getOut().view(-1)
-                    loss = criterion(output, torch.tensor(line[char+1], dtype=torch.float32).view(-1))
+                    loss = criterion(output, torch.tensor(line[char+1], dtype=torch.float64).view(-1))
                     optimizer.zero_grad()
                     loss.backward()
                     optimizer.step()
@@ -187,11 +193,10 @@ class Model(nn.Module):
         if sum(self.sprobs) == 0:
             start = icd[random.randint(0,vocab_size)]
         else:
-            sp = [i/sum(self.sprobs) for i in self.sprobs]
-            print(sp)
+            sp = [i/sum(self.sprobs[:-1]) for i in self.sprobs[:-1]]
             start = icd[np.random.choice(range(len(sp)), p=sp)]
             
-        last = torch.tensor(oneHott(start), dtype=torch.float32).view(1,-1)
+        last = torch.tensor(oneHott(start), dtype=torch.float64).view(1,-1)
         out = [start]
         for _ in range(100):
             self.model.forward(last)
@@ -207,13 +212,73 @@ class Model(nn.Module):
         print(out)
         print("".join(out))
 
-    def generateDecimal(self, message):
-        start = cid[message[0]]
-        onehots = [oneHott(i) for i in message[1:]]
-        decimal = 0
-        sp = [i / sum(self.sprobs) for i in self.sprobs]
+    def testDeterminism(self):
+        inp = torch.tensor([0,1,0,0], dtype=torch.float64).view(1,-1)
+        with torch.no_grad():
+            self.model.eval()
+            self.model.reset()
+            self.model.forward(inp)
+            a = self.model.getOut().view(-1).tolist()
+            self.model.reset()
+            self.model.forward(inp)
+            b = self.model.getOut().view(-1).tolist()
+            print(a)
+            print(b)
+            return a==b
 
-        return decimal
+    def generateDecimal(self, message):
+        with torch.no_grad():
+            self.model.eval()
+            self.model.reset()
+            last = cid[message[0]]
+            onehots = [oneHott(i) for i in message]
+            onehots.append(end)
+            print(onehots)
+            sp = [i / sum(self.sprobs) for i in self.sprobs]
+            print(sp)
+            decimal = sum(sp[:last])
+            print(decimal)
+            rang = sp[last]
+            for index, i in enumerate(onehots[1:]):
+                print("the for loop ran")
+                self.model.forward(torch.tensor(onehots[index-1], dtype=torch.float64).view(1,-1))
+                out = self.model.getOut().view(-1).tolist()
+                print(out)
+                b = sum(out[:np.argmax(i)])
+                decimal += b*rang
+                print(decimal)
+                rang *= out[np.argmax(i)]
+            return decimal
+        
+    def interpretDecimal(self, decimal):
+        with torch.no_grad():
+            self.model.reset()
+            outdistro = [i / sum(self.sprobs) for i in self.sprobs]
+            res = ""
+
+            for _ in range(100):
+                print(outdistro)
+                c = 0
+                ps = 0
+                for i in range(len(outdistro)):
+                    ps = sum(outdistro[:i])
+                    if ps <= decimal and ps + outdistro[i] > decimal:
+                        c = i
+                        break
+
+                decimal -= ps
+                decimal /= outdistro[c]
+                if c == vocab_size - 1:
+                    print("found end, breaking")
+                    break
+                res += icd[c]
+                print("Got a " + icd[c])
+                print(oneHott(icd[c]))
+                self.model.forward(torch.tensor(oneHott(icd[c]), dtype=torch.float64).view(1,-1))
+                outdistro = self.model.getOut().view(-1).tolist()
+
+            return res
+
 
         
 
@@ -223,6 +288,13 @@ num_epochs = 10
 
 model = Model(input_size, input_size, hidden_size)
 model.makeTest()
-while (input("Train?") == "y"):
-    model.trainSequence(5, inputy)
-    model.makeTest()
+# while (input("Train?") == "y"):
+model.trainSequence(1, inputy, graph=False)
+model.makeTest()
+
+# dec = model.generateDecimal("b")
+# print(dec)
+# outt = model.interpretDecimal(dec)
+# print(outt)
+
+print(model.testDeterminism())
