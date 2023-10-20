5a889e07c64e7fa51e9f358b357100075f95410e
Leorasz <scaevola1337@duck.com>
2023-10-20 07:30:09 -0600

    Make decimal more accurate, cleaned up code and added comments. Might come back and make it even better.

--- model.py
+++ model.py
@@ -1,23 +1,16 @@
-
 import random
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import numpy as np
+from decimal import Decimal, getcontext
 
 np.random.seed(1337)
 torch.manual_seed(1337)
 random.seed(1337)
+getcontext().prec = 100
 
-"""
-TODO
-
-Clean up code
-Better variable names
-Immediately convert to binary
-"""
 print("Finished imports")
 
 with open('input.txt', 'r', encoding='utf-8') as f:
@@ -25,19 +18,16 @@ with open('input.txt', 'r', encoding='utf-8') as f:
 
 tokens = sorted(list(set("".join(text))))
 vocab_size = len(tokens) + 1
-cid = { ch:i for i,ch in enumerate(tokens)}
+cid = { ch:i for i,ch in enumerate(tokens)} #make dicts to convert from index to character
 icd = { i:ch for i,ch in enumerate(tokens)}
 
-end = [0] * vocab_size
+end = [0] * vocab_size #making a token to signify the end
 end[-1] = 1
 
-encode = lambda s: [cid[c] for c in s]
-decode = lambda l: "".join([icd[i] for i in l])
-
-limit = 10000
+limit = 10000 #just so you don't have to train on the whole thing, optional
 text = text[:limit]
 inputy = []
-t = []
+textStore = []
 double = False
 wTFN = False
 recording = False
@@ -51,7 +41,7 @@ def oneHott(x):
     return res
 
 
-for i in text:
+for i in text: #this code breaks up the dialogue into a list of every thing they say, where each time a character speaks it's a new element, and the names and returns aren't included
     if i == "\n":
         if wTFN:
             wTFN = False
@@ -61,7 +51,7 @@ for i in text:
             wTFN = True
             record.append(end)
             inputy.append(record[1:-1])
-            t.append(rt[1:-1])
+            textStore.append(rt[1:-1])
             record = []
             rt = []
         double = not double
@@ -72,10 +62,10 @@ for i in text:
         rt.append(i) 
 
 inputy = inputy[1:]
-t = t[1:]
+textStore = textStore[1:]
 print("Finished data processing")
 
-class LSTM(nn.Module):
+class LSTM(nn.Module): #custom LSTM, just so I know exactly what's going on
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
 
@@ -116,11 +106,11 @@ class LSTM(nn.Module):
         return F.softmax(self.output(self.h_s), dim=1)
 
     def reset(self):
-        self.h_s.zero_()
+        self.h_s.zero_() #zero out the model
         self.c_s.zero_()
 
     def detach(self):
-        self.h_s.detach_()  # reset the model between sequences
+        self.h_s.detach_()
         self.c_s.detach_()
 
     def getHids(self):
@@ -135,7 +125,7 @@ class Model(nn.Module):
         self.model = LSTM(input_size, hidden_size, input_size)
         self.iis = input_size
         self.os = output_size
-        self.sprobs = [1e-7]*input_size
+        self.sprobs = [1e-7]*input_size #default epsilon so no divide by zero
 
     def trainSequence(
         self,
@@ -163,7 +153,7 @@ class Model(nn.Module):
                 start = True
                 for char in range(len(line) - 1):
                     self.model.forward(torch.tensor(line[char], dtype=torch.float64).view(1,-1))
-                    if start:
+                    if start: #the prediction for the start doesn't use the model at all since it's going off nothing, just a purely probabilistic start distribution
                         pos = np.argmax(line[char])
                         self.sprobs[pos] += 1
                         start = False
@@ -179,7 +169,7 @@ class Model(nn.Module):
 
                     loss_holder.append(loss.item())
                 self.model.reset()
-            mloss = np.mean(loss_holder)
+            mloss = np.mean(loss_holder) #average loss
             print(f"Epoch [{epoch+1}/{epochs}], loss is {mloss:.6f}")
             losses.append(mloss)
 
@@ -188,7 +178,7 @@ class Model(nn.Module):
             plt.show()
             plt.clf()
     
-    def makeTest(self):
+    def makeTest(self): #this uses the LSTM to generate language, just to gauge how accurate it is
         self.model.reset()
         if sum(self.sprobs) == 0:
             start = icd[random.randint(0,vocab_size)]
@@ -198,7 +188,7 @@ class Model(nn.Module):
             
         last = torch.tensor(oneHott(start), dtype=torch.float64).view(1,-1)
         out = [start]
-        for _ in range(100):
+        for _ in range(100): #just in case there's no end token
             self.model.forward(last)
             output = self.model.getOut().view(1,-1)
             a = output.view(-1).tolist()
@@ -212,7 +202,7 @@ class Model(nn.Module):
         print(out)
         print("".join(out))
 
-    def generateDecimal(self, message):
+    def generateDecimal(self, message): #this generates a binary decimal, which will be the message
         with torch.no_grad():
             self.model.eval()
             self.model.reset()
@@ -220,24 +210,26 @@ class Model(nn.Module):
             onehots = [oneHott(i) for i in message]
             onehots.append(end)
             sp = [i / sum(self.sprobs) for i in self.sprobs]
-            decimal = sum(sp[:last])
-            rang = sp[last]
-            self.m = 0
+            decimal = Decimal(sum(sp[:last])) #i use Decimal because the numbers can get pretty tiny and a float isn't always accurate enough
+            rang = Decimal(sp[last])
             for index, i in enumerate(onehots):
                 if index == 0:
                     continue
                 self.model.forward(torch.tensor(onehots[index-1], dtype=torch.float64).view(1,-1))
                 out = self.model.getOut().view(-1).tolist()
-                self.m = self.model.getHids()
                 b = sum(out[:np.argmax(i)])
-                decimal += b*rang
-                rang *= out[np.argmax(i)]
+                decimal += Decimal(b)*rang
+                rang *= Decimal(out[np.argmax(i)])
             res = []
-            nd = 0
-            v = 1
-            while nd < decimal or nd >= decimal + rang:
-
-                add = (1/2**v)
+            nd = Decimal(0)
+            v = 1 #the exponent of the 2
+            count = 0
+            while nd < decimal or nd >= decimal + rang: #here it convert the decimal to binary, within the given range of error
+                count += 1
+                if count >= 1000:
+                    print("Could not get enough precision")
+                    break #just so it doesn't loop forever, which it can do sometimes
+                add = Decimal(1/2**v)
                 if nd + add < decimal + rang:
                     nd += add
                     res.append(1)
@@ -247,22 +239,21 @@ class Model(nn.Module):
 
             return res
         
-    def interpretDecimal(self, decimal):
+    def interpretDecimal(self, decimal): #interprets the binary decimal
         with torch.no_grad():
-            o = 0
-            for index, i in enumerate(decimal):
-                o += float(i) * (1/(2**(index+1)))
+            o = Decimal(0)
+            for index, i in enumerate(decimal): #converts back to base 10 decimal
+                o += Decimal(float(i) * (1/(2**(index+1))))
             self.model.reset()
             outdistro = [i / sum(self.sprobs) for i in self.sprobs]
             res = ""
-            def midsearch(decimal, outidstro):
+            def midsearch(decimal, outidstro): #binary search algo to find correct spot in the distribution
                 l, r = 0, len(outidstro)
-
                 while l < r:
                     mid = (r-l)//2 + l
-                    prev = sum(outidstro[:mid])
+                    prev = Decimal(sum(outidstro[:mid]))
                     small = prev <= decimal
-                    big = prev + outidstro[mid] > decimal
+                    big = prev + Decimal(outidstro[mid]) > decimal
                     if small and big:
                         return mid, prev
                     if small:
@@ -273,11 +264,10 @@ class Model(nn.Module):
             for _ in range(100):
                 c, ps = midsearch(o, outdistro)
                 o -= ps
-                o /= outdistro[c]
+                o /= Decimal(outdistro[c])
                 if c == vocab_size - 1:
                     break
                 res += icd[c]
-                print("Got a " + icd[c])
                 self.model.forward(torch.tensor(oneHott(icd[c]), dtype=torch.float64).view(1,-1))
                 outdistro = self.model.getOut().view(-1).tolist()
 
@@ -292,9 +282,7 @@ num_epochs = 10
 
 model = Model(input_size, input_size, hidden_size)
 model.makeTest()
-model.trainSequence(1, inputy, graph=False)
-model.makeTest()
-model.trainSequence(1, inputy, graph=False)
+model.trainSequence(num_epochs, inputy)
 model.makeTest()
 
 
@@ -304,26 +292,12 @@ def log(x):
         res += 1
     return res
 
-a = 0
-for i in t[:5]:
-    print("New test")
+testlen = 5
+a = 0 #average store
+for i in textStore[:testlen]: #just testing the first couple lines to see how well it compresses
     n = len(i) * log(vocab_size)
     dec = model.generateDecimal(i)
-    if model.interpretDecimal(dec) != i:
-        print("There was an issue")
-        print(model.interpretDecimal(dec))
-        print(i)
     c = len(dec)
     a += (1-(c/n))*100
-a /= 5
+a /= testlen
 print("Compressed text was ", a, "% shorter than uncompressed")
-#print(model.testDeterminism())
-
-"""
-How to generate in only binary
-rang gets too small
-"""
-
-"""
-How to interpret in only binary
-"""
